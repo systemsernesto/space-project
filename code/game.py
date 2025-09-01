cb79198189d0ef334922c420d072ac9d05cb7fbe
ernesto <ernesto.systems@gmail.com>
2025-08-31 23:32:39 -0300

    Menu text add

--- code/game.py
+++ code/game.py
@@ -2,15 +2,17 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(576, 324))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
--- code/menu.py
+++ code/menu.py
@@ -7,9 +7,25 @@ class Menu:
     def __init__(self, window):
         self.window = window
         self.surf = pygame.image.load('./assets/menu.png')
-        self.rect = self.surf.get_rect(left=0,top=0)
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        self.window.blit(source=self.surf, dest=self.rect)
-        pygame.display.flip()
-        pass
+        pygame.mixer_music.load('./assets/menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(50, 'Space', COLOR_ORANGE, (WIN_WIDTH / 2, 70))
+            self.menu_text(50, 'Space', COLOR_ORANGE, (WIN_WIDTH / 2, 120))
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close windows
+                    quit()  # End pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
